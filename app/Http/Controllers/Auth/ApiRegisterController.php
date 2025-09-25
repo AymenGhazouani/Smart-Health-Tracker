cc9273382cf6a1620e2f75ef46d56c1dcc1e054c
jihedsaguer <92057955+jihedsaguer@users.noreply.github.com>
2025-09-25 16:53:22 +0200

    creating auth system with laravel/breeze  with session , token etc

---
+++ app/Http/Controllers/Auth/ApiRegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class ApiRegisterController extends Controller
+{
+    /**
+     * Handle an incoming API registration request.
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+}
